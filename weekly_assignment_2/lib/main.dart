80ffbbf97ad67ebf0f99e9b3b013710deea3a35f
adrian-szwejk <77693969+adrian-szwejk@users.noreply.github.com>
2022-10-02 15:42:46 -0500

    Update main.dart
    added comments

--- weekly_assignment_2/lib/main.dart
+++ weekly_assignment_2/lib/main.dart
@@ -27,14 +27,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //Creating a counter to know when to update text
   int _counter = 0;
+  '''Text Controllers are a variable we have to create to use TextFields 
+  and for users to change the text and see the changes in real time
+  '''
   TextEditingController firstNameController = TextEditingController();
   TextEditingController lastNameController = TextEditingController();
+  //Creating temp variables and then variables to set as the answers only after the button is pressed
   String firstNameTemp = '';
   String lastNameTemp = '';
   String firstName = '';
   String lastName = '';
-
+  
+  //Sets the name displayed on the screen to the temp name stored by the textField and increments the counter by 1
   void changeNameCounter() {
     setState(() {
       firstName = firstNameTemp;
@@ -54,13 +60,18 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            //Created a divider between the top and the textfield
             Divider(),
+            //Text field is a stateful widget that has a real-time updating box in which you can use your keyboard to type words
             TextField(
+              //Connected the firstName controller to the parameter in Textfield as we need a different controller for each field
               controller: firstNameController,
+              //Created decroation for the textField to have a more visible box as well as have a text indicating what the field is for
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'First name',
               ),
+              //onChange requires a function with a parameter. The 'text' parameter is what is being typed and onChanged updates the temp variable with it in live time
               onChanged: (text) {
                 setState(() {
                   firstNameTemp = text;
@@ -68,6 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             Divider(),
+            //Last name controller connected to a different text field to make it more distinct to users
             TextField(
               controller: lastNameController,
               decoration: const InputDecoration(
@@ -83,7 +95,9 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             Divider(),
+            //Created condition outside of text widget so that the name isn't displayed when nothing has been input yet
             if (_counter >= 1)
+              //Displays text widget with the first and last name concatenated with string interpolation
               Text(
                 'Hello $firstName $lastName',
                 style: Theme.of(context).textTheme.headline4,
@@ -91,6 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      //Floating action button used to update the name display as well as the counter only after being pressed
       floatingActionButton: FloatingActionButton(
         onPressed: changeNameCounter,
         tooltip: 'Increment',
