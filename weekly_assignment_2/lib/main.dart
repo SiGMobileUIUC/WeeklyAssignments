f335b41fb09e7237c437cb02dda921a332d3482b
Apoorva Aditya <apoorva9@illinois.edu>
2022-10-04 14:49:48 -0500

    updated the solution for weekly asssignment 2

--- weekly_assignment_2/lib/main.dart
+++ weekly_assignment_2/lib/main.dart
@@ -27,25 +27,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //Creating a counter to know when to update text
-  int _counter = 0;
-  '''Text Controllers are a variable we have to create to use TextFields 
-  and for users to change the text and see the changes in real time
-  '''
-  TextEditingController firstNameController = TextEditingController();
-  TextEditingController lastNameController = TextEditingController();
-  //Creating temp variables and then variables to set as the answers only after the button is pressed
-  String firstNameTemp = '';
-  String lastNameTemp = '';
-  String firstName = '';
-  String lastName = '';
-  
-  //Sets the name displayed on the screen to the temp name stored by the textField and increments the counter by 1
+  // Creating a flag variable to know when to update text
+  bool _displayText = true;
+
+  // Creating temp variables and then variables to set as the answers only after the button is pressed
+  String _firstNameTemp = '';
+  String _lastNameTemp = '';
+
+  String _firstName = '';
+  String _lastName = '';
+
+  // Sets the name displayed on the screen to the temp name stored by the textField and sets the displayText variable to true
   void changeNameCounter() {
     setState(() {
-      firstName = firstNameTemp;
-      lastName = lastNameTemp;
-      _counter++;
+      _firstName = _firstNameTemp;
+      _lastName = _lastNameTemp;
+      _displayText = true;
     });
   }
 
@@ -60,28 +57,25 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            //Created a divider between the top and the textfield
-            Divider(),
-            //Text field is a stateful widget that has a real-time updating box in which you can use your keyboard to type words
+            // Created a divider between the top and the textfield
+            const Divider(),
+            // Text field is a stateful widget that has a real-time updating box in which you can use your keyboard to type words
             TextField(
-              //Connected the firstName controller to the parameter in Textfield as we need a different controller for each field
-              controller: firstNameController,
-              //Created decroation for the textField to have a more visible box as well as have a text indicating what the field is for
+              // Created decroation for the textField to have a more visible box as well as have a text indicating what the field is for
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'First Name',
               ),
-              //onChange requires a function with a parameter. The 'text' parameter is what is being typed and onChanged updates the temp variable with it in live time
+              // onChange requires a function with a parameter. The 'text' parameter is what is being typed and onChanged updates the temp variable with it in live time
               onChanged: (text) {
                 setState(() {
-                  firstNameTemp = text;
+                  _firstNameTemp = text;
                 });
               },
             ),
-            Divider(),
-            //Last name controller connected to a different text field to make it more distinct to users
+            const Divider(),
+            // Last name controller connected to a different text field to make it more distinct to users
             TextField(
-              controller: lastNameController,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Last Name',
@@ -89,23 +83,23 @@ class _MyHomePageState extends State<MyHomePage> {
               onChanged: (text) {
                 setState(
                   () {
-                    lastNameTemp = text;
+                    _lastNameTemp = text;
                   },
                 );
               },
             ),
-            Divider(),
-            //Created condition outside of text widget so that the name isn't displayed when nothing has been input yet
-            if (_counter >= 1)
-              //Displays text widget with the first and last name concatenated with string interpolation
+            const Divider(),
+            // Created condition outside of text widget so that the name isn't displayed when nothing has been input yet
+            if (_displayText)
+              // Displays text widget with the first and last name concatenated with string interpolation
               Text(
-                'Hello $firstName $lastName',
+                'Hello $_firstName $_lastName',
                 style: Theme.of(context).textTheme.headline4,
               ),
           ],
         ),
       ),
-      //Floating action button used to update the name display as well as the counter only after being pressed
+      // Floating action button used to update the name display as well as the counter only after being pressed
       floatingActionButton: FloatingActionButton(
         onPressed: changeNameCounter,
         tooltip: 'Increment',
