0d2258578b8781c3a62d2bfe501e51016304d5eb
Apoorva Aditya <apoorva9@illinois.edu>
2022-10-04 16:15:04 -0500

    fixed the tooltip comment error and refactored to make it easier to read

--- weekly_assignment_1/lib/main.dart
+++ weekly_assignment_1/lib/main.dart
@@ -27,28 +27,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //Counter to keep track of the value on the screen
+  // Counter to keep track of the value on the screen
   int _counter = 0;
-   
-  //Function to increment that value of _counter by 1 and display that updated value on the screen
+
+  // Function to increment that value of _counter by 1 and display that updated value on the screen
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
   }
-  
-  //Function to subtract that value of _counter by 1 and display that updated value on the screen
+
+  // Function to subtract that value of _counter by 1 and display that updated value on the screen
   void _decrementCounter() {
     setState(() {
       _counter--;
     });
   }
 
-  //Function to set the value of _counter to 0 and display the updated value on the screen
+  // Function to set the value of _counter to 0 and display the updated value on the screen
   void _zeroCounter() {
     setState(() {
       _counter = 0;
     });
+  }
+
+  // Function to return the correct String depending on the _counter
+  String fizzBuzz() {
+    // If _counter is divisible by 5 & 3 it displays 'fizzbuzz'
+    if (_counter % 5 == 0 && _counter % 3 == 0) {
+      return "fizzbuzz";
+    }
+    // If _counter is divisible by only 3 it displays 'fizz'
+    else if (_counter % 3 == 0) {
+      return "fizz";
+    }
+    // If _counter is divisible by only 5 it displays 'buzz'
+    else if (_counter % 5 == 0) {
+      return "buzz";
+    }
+    // If _counter is not divisible by 5 nor 3, it displays '----'
+    else {
+      return "----";
+    }
   }
 
   @override
@@ -60,56 +80,42 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          //Children is a list of widget that we are organizing verically in the column
+          // Children is a list of widget that we are organizing verically in the column
           children: <Widget>[
-            //This a text widget to display the updating value of _counter
+            // This a text widget to display the updating value of _counter
             Text(
-              //String interprelation is used by haveing a $ in front of _counter to display the updating counter in a text widget
+              // String interprelation is used by haveing a $ in front of _counter to display the updating counter in a text widget
               '$_counter',
-              //Using default themes that come with flutter
+              // Using default themes that come with flutter
               style: Theme.of(context).textTheme.headline4,
             ),
-            //Text widget to display either 'fizz', 'buzz', or 'fizzbuzz' depending on the value of _counter
-            Text((() {
-              //Created a function inside the Text widget to return certain values to display
-              if (_counter % 5 == 0 && _counter % 3 == 0) {
-                //If _counter is divisible by 5 & 3 it displays 'fizzbuzz'
-                return "fizzbuzz";
-              } else if (_counter % 3 == 0) {
-                //If _counter is divisible by only 3 it displays 'fizz'
-                return "fizz";
-              } else if (_counter % 5 == 0) {
-                //If _counter is divisible by only 5 it displays 'buzz'
-                return "buzz";
-              } else
-                return "----";
-            })()),
-            //Divided to create space between the text displaying fizzbuzz and the buttons
-            Divider(),
-            //Row to organize the buttons horizontally
+            // Text widget to display either 'fizz', 'buzz', or 'fizzbuzz' depending on the value of _counter
+            Text(fizzBuzz()),
+            // Divided to create space between the text displaying fizzbuzz and the buttons
+            const Divider(),
+            // Row to organize the buttons horizontally
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //Added space between the buttons on the main axis(horizontal)
+              // Added space between the buttons on the main axis (horizontal)
               children: [
-                //Made 3 floating action buttons for easier setup
+                // Made 3 floating action buttons for easier setup
                 FloatingActionButton(
-                  //When the button with a 0 symbol is pressed it calls the _zeroCounter function that sets the number of screen to 0
+                  // When the button with a 0 symbol is pressed it calls the _zeroCounter function that sets the number of screen to 0
                   onPressed: _zeroCounter,
-                  '''Tooltip is not used here but it's the label that the system uses to display what a button does when you hold it or 
-                  what a phone says when it reads everything on screen for blind people in the disability mode in flutter
-                  '''
+                  //  Tooltip is not used here but it's the label that the system uses to display what a button does when you hold it or
+                  //  what a phone says when it reads everything on screen for blind people in the disability mode in flutter
                   tooltip: 'Zero',
-                  //Used default icons that come with flutter through Icons.add, or Icons.remove, etc.
+                  // Used default icons that come with flutter through Icons.add, or Icons.remove, etc.
                   child: const Icon(Icons.exposure_zero),
                 ),
                 FloatingActionButton(
-                   //When the button with a '-' symbol is pressed it calls the _decrementCounter function that decreases the number of the screen by 1
+                  // When the button with a '-' symbol is pressed it calls the _decrementCounter function that decreases the number of the screen by 1
                   onPressed: _decrementCounter,
                   tooltip: 'Decrement',
                   child: const Icon(Icons.remove),
                 ),
                 FloatingActionButton(
-                  //When the button with a '+' symbol is pressed it calls the _decrementCounter function that increases the number of the screen by 1
+                  // When the button with a '+' symbol is pressed it calls the _decrementCounter function that increases the number of the screen by 1
                   onPressed: _incrementCounter,
                   tooltip: 'Increment',
                   child: const Icon(Icons.add),
