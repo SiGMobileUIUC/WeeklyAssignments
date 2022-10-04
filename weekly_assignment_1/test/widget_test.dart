fe2f00704fd6bbf697cf63653b3314dc18b75ec6
Apoorva Aditya <apoorva9@illinois.edu>
2022-10-04 16:29:36 -0500

    added tests for weekly assignment 1

--- weekly_assignment_1/test/widget_test.dart
+++ weekly_assignment_1/test/widget_test.dart
@@ -7,11 +7,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:weekly_assignment_1/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments correctly', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
@@ -26,5 +25,85 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  });
+
+  testWidgets('Counter decrements correctly', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('-1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.pump();
+
+    // Verify that our counter has decremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('-1'), findsOneWidget);
+  });
+
+  testWidgets('Counter resets to zero correctly', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Tap the '0' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.exposure_zero));
+    await tester.pump();
+
+    // Verify that our counter is at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+  });
+
+  testWidgets('Fizz shows up when the counter is divisible by 3', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    for (int i = 0; i < 3; i++) {
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+    }
+
+    // Verify that fizz shows up when counter is 3
+    expect(find.text('fizz'), findsOneWidget);
+  });
+
+  testWidgets('Buzz shows up when the counter is divisible by 5', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    for (int i = 0; i < 5; i++) {
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+    }
+
+    // Verify that fizz shows up when counter is 3
+    expect(find.text('buzz'), findsOneWidget);
+  });
+
+  testWidgets('FizzBuzz shows up when the counter is divisible by 3 & 5', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    for (int i = 0; i < 15; i++) {
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+    }
+
+    // Verify that fizz shows up when counter is 3
+    expect(find.text('fizzbuzz'), findsOneWidget);
   });
 }
