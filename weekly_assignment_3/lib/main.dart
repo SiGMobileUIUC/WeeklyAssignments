0cb5dee31717f12ce867919858d4d42b798898f1
Apoorva Aditya <apoorva9@illinois.edu>
2022-10-09 19:03:45 -0500

    added the solution for weekly assignment 3

--- weekly_assignment_3/lib/main.dart
+++ weekly_assignment_3/lib/main.dart
@@ -13,33 +13,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Weekly Assignment 3'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +30,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // List to keep track of which indices are still in the list view
+  final List<int> _listTileIndices = List.generate(12, (index) => index);
 
-  void _incrementCounter() {
+  // Function to remove the list tile at the given index
+  void _removeAtIndex(int listTileIndexToRemove) {
+    // Using setState to tell Flutter to rebuild the widget and UI
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      // Remove the listTileIndex at the given index
+      _listTileIndices.removeAt(listTileIndexToRemove);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      // Using a ListView.builder widget so that we can use the index of each ListTile being built to customized that
+      // ListTile
+      body: ListView.builder(
+        // This anonymous function gives you an updated BuildContext (you don't have to worry about that right now but
+        // you can look up what it is) and the index of the current item being built. It has to return a Widget which
+        // the ListView will use to create its children
+        itemBuilder: (context, listTileIndex) => ListTile(
+          // Widget used to create the circle at the start of the ListTile. Can be used for other things such as
+          leading: CircleAvatar(backgroundColor: Colors.primaries[listTileIndex % Colors.primaries.length]),
+          // The title text of the ListTile
+          title: const Text('I am a ListTile'),
+          // The text below the title of the ListTile
+          subtitle: Text('Index: ${_listTileIndices[listTileIndex]}'),
+          // The delete button at the end of the ListTile
+          trailing: IconButton(
+            // Call the _removeAtIndex function when button is pressed with the index of the current ListTile
+            onPressed: () {
+              _removeAtIndex(listTileIndex);
+            },
+            icon: const Icon(Icons.delete),
+          ),
         ),
+        // Tells the ListView how many ListTiles to build
+        itemCount: _listTileIndices.length,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
