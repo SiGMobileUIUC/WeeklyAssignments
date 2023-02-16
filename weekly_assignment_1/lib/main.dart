f970af144239eb9a86b9c7cf3ef265d2cc19408a
Hirish Chandrasekaran <hirish@ucsb.edu>
2023-02-16 12:18:23 -0600

    Assignment 1 Done

--- weekly_assignment_1/lib/main.dart
+++ weekly_assignment_1/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -49,6 +49,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _buzz = '--';
 
   void _incrementCounter() {
     setState(() {
@@ -58,8 +59,23 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      _buzz = '--';
+      if (_counter % 3 == 0)
+      {
+        _buzz = 'Fizz';
+      }
+      if (_counter % 5 == 0)
+      {
+        _buzz = 'Buzz';
+      }
+      if (_counter % 3 == 0 && _counter % 5 == 0)
+      {
+        _buzz = 'FizzBuzz';
+      }
     });
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -101,6 +117,9 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              _buzz,
             ),
           ],
         ),
